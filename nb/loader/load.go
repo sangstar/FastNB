bae1da48ac938d025430c45d9ead709012967d10
Sanger Steel <sangersteel@gmail.com>
2024-02-11 14:37:52 -0500

    feat: Speed up processing with concurrency
    Allows CSV file to be parsed by multiple workers concurrently
    to bolster processing time for larger files

--- nb/loader/load.go
+++ nb/loader/load.go
@@ -7,15 +7,14 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"strconv"
-	"io"
-	"fmt"
 	"strings"
+	"sync"
 )
-
-var ChunkSize int = 5
 
 type Dataset struct {
 	Features [][][]int32
@@ -26,23 +25,90 @@ type FreqCount struct {
 	CountMap map[string]int
 }
 
+type FileLoader struct {
+	file *os.File
+}
+
 type FreqCounts []FreqCount
+
+const filePath string = "../data.csv"
+
+func NewFileLoader(filePath string) (*FileLoader, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	return &FileLoader{file: file}, nil
+}
+
+func (fl *FileLoader) Close() error {
+	return fl.file.Close()
+}
+
+func (fl *FileLoader) SeekStart() error {
+	_, err := fl.file.Seek(0, 0) // Seek to the start of the file
+	return err
+}
+
+func (fl *FileLoader) NewReader() *csv.Reader {
+	return csv.NewReader(fl.file)
+}
+
+func (fl *FileLoader) getNumLines() (NumLines int, err error) {
+	defer func() {
+		err := fl.SeekStart()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	var i = 0
+
+	reader := fl.NewReader()
+
+	for {
+		_, err := reader.Read()
+		if err == io.EOF {
+			return i, nil
+		} else if err != nil {
+			return 0, err
+		} else {
+			i += 1
+		}
+	}
+}
 
 // ReadChunk returns a slice of a Dataset of chunk size `size`.
 // Assumes dataset is in CSV format, and that features and classes
 // are defined in a specific way
-func (dataset *Dataset) ReadChunk(reader *csv.Reader, ChunkSize int) (Dataset, error) {
+func (dataset *Dataset) ReadChunk(dataFile *FileLoader, ChunkSize int, Pos int) (Dataset, error) {
 	var features [][][]int32
 	var classes []uint16
+
+	defer func() {
+		err := dataFile.SeekStart()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	reader := dataFile.NewReader()
+
+	for i := 0; i < Pos; i++ {
+		_, err := reader.Read()
+		if err != nil {
+			return Dataset{}, err
+		}
+	}
 
 	for i := 0; i < ChunkSize; i++ {
 		var feature [][]int32
 		record, err := reader.Read()
-		log.Println("Read record ", record)
+		log.Println("Read record", record)
 		if err != nil {
 			if err == io.EOF {
 				log.Println("Features at EOF", features)
-				return Dataset{features, classes}, err
+				return Dataset{Features: features, Classes: classes}, err
 			}
 			log.Println(err)
 			return Dataset{}, err
@@ -59,10 +125,8 @@ func (dataset *Dataset) ReadChunk(reader *csv.Reader, ChunkSize int) (Dataset, e
 			log.Println(err)
 			return Dataset{}, err
 		}
-		log.Println("Appending to features:", feature)
 		features = append(features, feature)
 		classes = append(classes, uint16(class))
-		log.Println("Feature array is now ", features)
 	}
 
 	return Dataset{features, classes}, nil
@@ -70,32 +134,68 @@ func (dataset *Dataset) ReadChunk(reader *csv.Reader, ChunkSize int) (Dataset, e
 
 // Load loads the dataset from a CSV file and reads a chunk of it.
 func Load() (FreqCount, error) {
-	var dataChunk Dataset = Dataset{}
+	var freqCounts FreqCounts
+	var ChunkSize = 3
+	var wg sync.WaitGroup
 
-	file, err := os.Open("../data.csv")
+	dataFile, err := NewFileLoader(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	reader := csv.NewReader(file)
 
-	var freqCounts FreqCounts
-	for {
-		dataChunk, readErr := dataChunk.ReadChunk(reader, ChunkSize)
-		if (readErr != nil && readErr != io.EOF) {
-			log.Fatal(err)
-		}
-		log.Println("Instantiated dataChunk", dataChunk)
-		Counts, err := dataChunk.ToCounts()
-		log.Println("Counts are ", Counts)
-		if err != nil {
-			log.Fatal(err)
-		}
-		freqCounts = append(freqCounts, Counts)
-		if readErr == io.EOF {
-			break
-		}
+	numLines, err := dataFile.getNumLines()
+	if err != nil {
+		log.Fatal(err)
 	}
-	log.Println("freqCounts: ", freqCounts)
+	log.Println(numLines)
+
+	remainder := numLines % ChunkSize
+
+	var numWorkers int
+	if remainder == 0 {
+		numWorkers = numLines / ChunkSize
+	} else {
+		numWorkers = numLines/ChunkSize + remainder
+	}
+
+	Data := Dataset{}
+	countsChan := make(chan FreqCount, numWorkers)
+
+	wg.Add(numWorkers)
+
+	for i := 0; i < numWorkers; i++ {
+		chunkSize := ChunkSize
+		if i == numWorkers-1 && remainder != 0 {
+			chunkSize = remainder
+		}
+		loadedChunk, err := Data.ReadChunk(dataFile, chunkSize, i*ChunkSize)
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
+		go func(worker int, dataChunk Dataset) {
+			defer wg.Done()
+			counts, err := dataChunk.ToCounts()
+			if err != nil {
+				log.Fatal(err)
+			}
+			countsChan <- counts
+		}(i, loadedChunk)
+	}
+
+	go func() {
+		wg.Wait()
+		close(countsChan)
+	}()
+
+	freqCounts = FreqCounts{}
+	for count := range countsChan {
+		freqCounts = append(freqCounts, count)
+	}
+
+	err = dataFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	Combined, err := freqCounts.Combine()
 	if err != nil {
@@ -105,7 +205,6 @@ func Load() (FreqCount, error) {
 	return Combined, nil
 }
 
-
 const separator = "|" // Signifying "such that", such as P(w|c)
 
 // ToCounts converts a Dataset into a FreqCount, which gives the
@@ -113,6 +212,7 @@ const separator = "|" // Signifying "such that", such as P(w|c)
 func (dataset *Dataset) ToCounts() (FreqCount, error) {
 	mapper := make(map[string]int)
 
+	log.Println(dataset)
 	for i, class := range dataset.Classes {
 		sClass := fmt.Sprintf("%d", class)
 		for _, featureList := range dataset.Features[i] {
@@ -128,10 +228,10 @@ func (dataset *Dataset) ToCounts() (FreqCount, error) {
 }
 
 // Combine concatenates a FreqCounts struct into a single
-// FreqCount with all of the counts summed up per feature
+// FreqCount with all the counts summed up per feature
 func (freqCounts FreqCounts) Combine() (FreqCount, error) {
 	if len(freqCounts) == 0 {
-		return FreqCount{}, errors.New("No frequency counts to combine")
+		return FreqCount{}, errors.New("no frequency counts to combine")
 	}
 
 	result := FreqCount{CountMap: make(map[string]int)}
